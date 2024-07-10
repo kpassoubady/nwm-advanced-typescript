5c73904a24239ce23d431fc4047e3fbb75c3240a
kangs passoubady <kpassoubady@gmail.com>
2024-07-10 12:06:26 -0400

    Updated error info

--- src/Day1/2-Advanced-Generics/2.2-Using-Constraints-with-Generics/1-GetLength.ts
+++ src/Day1/2-Advanced-Generics/2.2-Using-Constraints-with-Generics/1-GetLength.ts
@@ -14,5 +14,10 @@ console.log(length1); // 5
 let length2 = getLength([1, 2, 3]);
 console.log(length2); // 3
 
-let length3 = getLength(["kangs","alex","addy","max"]); // Error: Type '10' does not have a property 'length'
+let length3 = getLength(["kangs","alex","addy","max"]); 
 console.log(length3); // 4
+
+//let lenght4 = getLength(10); // Argument of type '10' is not assignable to parameter of type 'Lengthwise'.ts(2345)
+
+let length5 = getLength({ length: 10, value: 3 });
+console.log(length5);
--- src/Day1/2-Advanced-Generics/2.2-Using-Constraints-with-Generics/4-PrintName.ts
+++ src/Day1/2-Advanced-Generics/2.2-Using-Constraints-with-Generics/4-PrintName.ts
@@ -7,5 +7,7 @@ function printName<T extends { name: string }>(obj: T) {
 printName({ name: "Kangs", age: 25 }); // Output: "Kangs"
 
 // Compilation error: Property 'name' is missing in type '{ firstName: string; }' but required in type '{ name: string; }'
-//printName({ firstName: "Kavya" }); 
+// printName({ firstName: "Kavya" }); 
+// Argument of type '{ firstName: string; }' is not assignable to parameter of type '{ name: string; }'.
+// Object literal may only specify known properties, and 'firstName' does not exist in type '{ name: string; }'.ts(2345)
 printName({ name: "Kavya", user: "Andres", lastName: "Sam" }); 
