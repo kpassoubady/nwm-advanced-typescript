7b4653b5c67666bef1d1601d43dbf126e8d12f96
kangs passoubady <kpassoubady@gmail.com>
2024-07-10 11:40:59 -0400

    updated length example

--- src/Day1/2-Advanced-Generics/2.1-Overview-of-TypeScript-Generics/1-GenericPrintFunction.ts
+++ src/Day1/2-Advanced-Generics/2.1-Overview-of-TypeScript-Generics/1-GenericPrintFunction.ts
@@ -8,4 +8,7 @@ let output1 = print<string>("welcome");
 console.log(output1); // "welcome"
 
 let output2 = print<number>(20);
+
 console.log(output2); // 20
+let output3 = print<boolean>(10 > 20 ? true : false);
+console.log(output3);
--- src/Day1/2-Advanced-Generics/2.1-Overview-of-TypeScript-Generics/3-GetLengthOfArray.ts
+++ src/Day1/2-Advanced-Generics/2.1-Overview-of-TypeScript-Generics/3-GetLengthOfArray.ts
@@ -7,3 +7,5 @@ function getLengthOfArray<T>(arg: T[]): T[] {
 
 let myArr = getLengthOfArray<number>([1, 2, 3, 4, 5]); // Output: 5
 let myString = getLengthOfArray<string>(["1", "2", "3", "4", "5", "6"]); // Output: 6
+let myBool = getLengthOfArray<boolean>([true, false, true, false, true, false]);
+let myObj = getLengthOfArray<object>([{ a: 1 }, { b: 2 }, { c: 3 }]);
