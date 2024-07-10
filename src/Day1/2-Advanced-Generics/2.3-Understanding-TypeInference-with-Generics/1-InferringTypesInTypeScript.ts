ff6817ed995a2620558d96bd98a76378f1c72c2e
kangs passoubady <kpassoubady@gmail.com>
2024-07-10 12:16:00 -0400

    Example updated with error type

--- src/Day1/2-Advanced-Generics/2.3-Understanding-TypeInference-with-Generics/1-InferringTypesInTypeScript.ts
+++ src/Day1/2-Advanced-Generics/2.3-Understanding-TypeInference-with-Generics/1-InferringTypesInTypeScript.ts
@@ -4,6 +4,8 @@ let name = "Kangs"; // inferred as string
 let age = 30; // inferred as number
 let hasHobbies = true; // inferred as boolean
 
+// name = 12; // Type '12' is not assignable to type 'string'.ts(2322)
+
 function greet(name: string, age: number, hasHobbies:boolean) {
   console.log(`Hello, ${name}!`);
   console.log(`Your age is, ${age}!`);
@@ -15,3 +17,5 @@ function greet(name: string, age: number, hasHobbies:boolean) {
 greet(name,age,hasHobbies);
 greet("Kavya",12,false);
 greet('Melissa',24,true);
+greet('Paul',30,true);
+// greet('Sam',"40",false);
