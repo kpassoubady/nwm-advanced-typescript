d3243fe063200acbe8e0940601119ca4b4158ee1
kangs passoubady <kpassoubady@gmail.com>
2024-07-11 10:36:36 -0400

    added more logs

--- src/Day2/7-Functional-Composition/7.1-Functional-Composition/3-ComposeFunctionAddAndMultiply.ts
+++ src/Day2/7-Functional-Composition/7.1-Functional-Composition/3-ComposeFunctionAddAndMultiply.ts
@@ -1,15 +1,19 @@
 export {}
 
 function add(a: number, b: number): number {
+  console.log(`Adding ${a} and ${b}...`);
   return a + b;
 }
 
 function multiply(c: number, d: number): number {
+  console.log(`Multiplying ${c} and ${d}...`);
   return c * d;
 }
 
 // Function composition example
 function compose(f: (x: number, y: number) => number, g: (z: number, w: number) => number): (a: number, b: number) => number {
+  console.log("Composing functions...");
+  console.log(`f: ${f.name}, g: ${g.name}`);
   return (a: number, b: number) => f(g(a, b), g(a, b));
 }
 
